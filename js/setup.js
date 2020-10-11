a2a81f40f07f0d7e972ad0e77cccf34b51cffd1b
Olga Eltsova <olga.ellcova@gmail.com>
2020-10-11 22:44:19 +0500

    Описаны сценарии: открытие/закрытие окна настройки, валидация ввод имени, изменение цветов плаща, глаз, файербола по нажатию, отправка формы

--- js/setup.js
+++ js/setup.js
@@ -34,13 +34,72 @@ const Wizard = {
     `blue`,
     `yellow`,
     `green`
+  ],
+  FIREBALL_COLORS: [
+    `#ee4830`,
+    `#30a8ee`,
+    `#5ce6c0`,
+    `#e848d5`,
+    `#e6e848`
   ]
 };
 
 const WIZARDS_NUMBER = 4;
 
 const setupWindow = document.querySelector(`.setup`);
-setupWindow.classList.remove(`hidden`);
+const setupOpen = document.querySelector(`.setup-open`);
+const setupClose = setupWindow.querySelector(`.setup-close`);
+
+/**
+ * Закрывает окно настроек при нажатии кнопки Esc
+ * @param {event} evt - событие
+ */
+const onSetupWindowEscPress = function (evt) {
+  if (evt.key === `Escape`) {
+    evt.preventDefault();
+    closeSetupWindow();
+  }
+};
+
+/**
+ * Открывает окно настроек, добавляет слушитель события keydown
+ * @param {event} evt - событие
+ */
+const openSetupWindow = function () {
+  setupWindow.classList.remove(`hidden`);
+
+  document.addEventListener(`keydown`, onSetupWindowEscPress);
+};
+
+/**
+ * Закрывает окно настроек, удаляет слушитель события keydown
+ * @param {event} evt - событие
+ */
+const closeSetupWindow = function () {
+  setupWindow.classList.add(`hidden`);
+
+  document.removeEventListener(`keydown`, onSetupWindowEscPress);
+};
+
+setupOpen.addEventListener(`click`, function () {
+  openSetupWindow();
+});
+
+setupOpen.addEventListener(`keydown`, function (evt) {
+  if (evt.key === `Enter`) {
+    openSetupWindow();
+  }
+});
+
+setupClose.addEventListener(`click`, function () {
+  closeSetupWindow();
+});
+
+setupClose.addEventListener(`keydown`, function (evt) {
+  if (evt.key === `Enter`) {
+    closeSetupWindow();
+  }
+});
 
 const setupSimilar = setupWindow.querySelector(`.setup-similar`);
 setupSimilar.classList.remove(`hidden`);
@@ -87,10 +146,47 @@ const renderWizard = function (wizard) {
   return similarWizard;
 };
 
+const wizards = getWizardsArray(WIZARDS_NUMBER);
+
 const fragment = document.createDocumentFragment();
-for (let i = 0; i < getWizardsArray(WIZARDS_NUMBER).length; i++) {
-  fragment.appendChild(renderWizard(getWizardsArray(WIZARDS_NUMBER)[i]));
+for (let i = 0; i < wizards.length; i++) {
+  fragment.appendChild(renderWizard(wizards[i]));
 }
 
 const similarList = setupSimilar.querySelector(`.setup-similar-list`);
 similarList.appendChild(fragment);
+
+const wizardCoat = document.querySelector(`.wizard-coat`);
+const wizardEyes = document.querySelector(`.wizard-eyes`);
+const wizardFireball = document.querySelector(`.setup-fireball-wrap`);
+const coatColorInput = document.querySelector(`input[name=coat-color]`);
+const eyesColorInput = document.querySelector(`input[name=eyes-color]`);
+const fireballColorInput = document.querySelector(`input[name=fireball-color]`);
+
+/**
+ * Задает рандомный цвет элемента из массива и записывает его в соответсвующий этому элементу input
+ * @param {Object} element - DOM-элемент (svg или div)
+ * @param {Object} input - input, соответсвующий DOM-элементу
+ * @param {Array} colors - массив возможных цветов
+*/
+const setRandomColor = function (element, input, colors) {
+  let color = getRandomElement(colors);
+  if (element.nodeName === `DIV`) {
+    element.style.backgroundColor = color;
+  } else {
+    element.style.fill = color;
+  }
+  input.value = color;
+};
+
+wizardCoat.addEventListener(`click`, function () {
+  setRandomColor(wizardCoat, coatColorInput, Wizard.COAT_COLORS);
+});
+
+wizardEyes.addEventListener(`click`, function () {
+  setRandomColor(wizardEyes, eyesColorInput, Wizard.EYES_COLORS);
+});
+
+wizardFireball.addEventListener(`click`, function () {
+  setRandomColor(wizardFireball, fireballColorInput, Wizard.FIREBALL_COLORS);
+});
